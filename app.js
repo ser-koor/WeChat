da3f59b35f6a68df4043143f2a6ccdd44d08e5dd
ser-koor <1684398809@qq.com>
2022-10-22 18:39:11 +0800

    分享和登录

--- app.js
+++ app.js
@@ -1,19 +1,66 @@
 // app.js
+const TOKEN = 'token'
 App({
+  globalData: {
+    token: ''
+  },
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
+  //从缓存中取出token
+  const token = wx.getStorageSync(TOKEN);
+  //判断token是否有值
+  if (token && token !== 0) {
+    //验证token是否过期
+    this.check_token(token)
+  } else {
+    this.login();
+  }
+   
+  },
+  check_token(token) {
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      header: {
+        token
+      },
+      method: 'post',
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (!res.data.errCode) {
+          //token有效
+          console.log('token有效');
+          this.globalData.token = token;
+        } else {
+          this.login()
+        }
+      },
+      fail: err => {
+        console.log(err);
       }
     })
   },
-  globalData: {
-    userInfo: null
+  login() {
+    console.log('执行登录操作');
+    wx.login({
+      success: res => {
+        //获取code
+        const code = res.code;
+       //将Ccode发送给服务器
+       wx.request({
+         url: 'http://123.207.32.32:3000/login',
+         method: 'post',
+         data: {
+           code
+         },
+         success: res => {
+           //取出token
+           const token = res.data.token;
+           //将token保存到gglobalData中
+           this.globalData.token = token;
+           //将token进行本地存储
+           wx.setStorageSync(TOKEN, token)
+         }
+       })
+      }
+    })    
   }
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,53 @@ Page({
   data: {
 
   },
-
+  handleShowToaest() {
+    wx.showToast({
+      title: '支付成功',
+      duration: 3000,//显示时间 单位 ms
+      icon: 'success',//显示图标
+      mask: true,//遮罩层
+    })
+  },
+  handleShowModal() {
+    wx.showModal({
+      title: '主题',
+      content: '我是内容，哈哈哈',
+      cancelColor: '#f02121',
+      confirmColor: '#21f021',
+      cancelText: '退出',
+      showCancel: true,//是否显示取消按钮
+      success: res => {
+        if (res.cancel) {
+          console.log('你取消了');
+        }
+        if (res.confirm) {
+          console.log('你同意了');
+        }
+      }
+    })
+  },
+  handleShowLoding() {
+    wx.showLoading({
+      title: '加载中ing',
+      mask: true
+    })
+    //加个定时器取消loading界面
+    setTimeout(() => {
+      wx.hideLoading({
+        success: (res) => {console.log(res);},
+      })
+    }, 1000);
+  },
+  handleShowAction() {
+    wx.showActionSheet({
+      itemList: ['相机', '相册'],
+      success: res => {
+        console.log(res.tapIndex);
+      }
+    })
+  },
+ 
   /**
    * 生命周期函数--监听页面加载
    */
@@ -19,14 +65,14 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    // console.log('ready');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    // console.log('show');
   },
 
   /**
@@ -61,6 +107,10 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage() {
-
+    return {
+      title: '分享页面',
+      path: 'pages/logs/logs',
+      imageUrl: 'https://s10.mogucdn.com/mlcdn/c45406/180917_0hgle1e2c350a57ekhbj4f10a6b03_750x390.jpg'
+    }
   }
 })
