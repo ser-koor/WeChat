d9ebb446aadfa127a771112f461430a518017848
ser-koor <1684398809@qq.com>
2022-10-22 15:01:54 +0800

    网络请求相关

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,6 @@
 // pages/index/index.js
+import request from '../../service/network'
+
 Page({
 
   /**
@@ -12,7 +14,38 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    request({
+      url: 'http://152.136.185.210:7878/api/hy66/home/multidata'
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
 
+    // wx.request({
+    //   url: 'http://152.136.185.210:7878/api/hy66/home/multidata',
+    //   success: (res) => {
+    //     console.log(res);
+    //   }
+    // })
+    
+    // post
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: 'post',
+    //   data: {
+    //     name: '666',
+    //     id: 1
+    //   },
+    //   //成功回调
+    //   success: (res) => {
+    //     console.log(res);
+    //   },
+    //   //失败回调
+    //   fail: err => {
+    //     console.log(err);
+    //   }
+    // })
   },
 
   /**
